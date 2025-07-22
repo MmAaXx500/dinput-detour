7154a747cb8ae0fcd7cb7d71611414d06e494fb3
MmAaXx500 <viktor.balogh45@gmail.com>
2025-07-22 12:55:56 +0200

    cleanup direct_input_effect.h

--- dinput-detour/direct_input_effect.h
+++ dinput-detour/direct_input_effect.h
@@ -2,27 +2,6 @@
 
 #include <dinput.h>
 
-HRESULT WINAPI RoutedDirectInputEffectGetParameters(
-    LPDIRECTINPUTEFFECT lpDiEffect, LPDIEFFECT peff, DWORD dwFlags);
-
-HRESULT WINAPI RoutedDirectInputEffectSetParameters(
-    LPDIRECTINPUTEFFECT lpDiEffect, LPCDIEFFECT peff, DWORD dwFlags);
-
-HRESULT WINAPI RoutedDirectInputEffectStart(LPDIRECTINPUTEFFECT lpDiEffect,
-                                            DWORD dwIterations, DWORD dwFlags);
-
-HRESULT WINAPI RoutedDirectInputEffectStop(LPDIRECTINPUTEFFECT lpDiEffect);
-
-HRESULT WINAPI RoutedDirectInputEffectGetEffectStatus(
-    LPDIRECTINPUTEFFECT lpDiEffect, LPDWORD pdwFlags);
-
-HRESULT WINAPI RoutedDirectInputEffectDownload(LPDIRECTINPUTEFFECT lpDiEffect);
-
-HRESULT WINAPI RoutedDirectInputEffectUnload(LPDIRECTINPUTEFFECT lpDiEffect);
-
-HRESULT WINAPI RoutedDirectInputEffectEscape(LPDIRECTINPUTEFFECT lpDiEffect,
-                                             LPDIEFFESCAPE pesc);
-
 /**
  * Attach to DirectInputEffect functions
  *
